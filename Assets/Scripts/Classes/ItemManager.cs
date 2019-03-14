2d87f76e3a777ab8d0422578f2ff47dd0ab7f037
M-T-Asagi <t.asagi00+github@gmail.com>
2019-03-15 01:18:02 +0900

    Itemを取得できない問題を修正

--- Assets/Scripts/Classes/ItemManager.cs
+++ Assets/Scripts/Classes/ItemManager.cs
@@ -9,9 +9,23 @@ public class ItemManager : MonoBehaviour, IPointerClickHandler
     int id = 0;
     public int ID { get { return id; } }
 
+    [SerializeField]
+    ClickToMove clickToMove = null;
+
+    [SerializeField]
+    LayerMask layerMask = new LayerMask();
+
     public void OnPointerClick(PointerEventData eventData)
     {
         transform.parent.GetComponent<ClickedItemManager>().clickedItem = this;
+        RaycastHit raycastHit = new RaycastHit();
+        if(Physics.Raycast(((PointerEventData)eventData).pointerCurrentRaycast.worldPosition, Vector3.down, out raycastHit, 1))
+        {
+            clickToMove.SetNavigateTo(raycastHit.point);
+        } else
+        {
+            clickToMove.SetNavigateTo(((PointerEventData)eventData).pointerCurrentRaycast.worldPosition);
+        }
     }
 
     public void RemoveItem()
