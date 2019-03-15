3c76f21519052c325a749ce6367b6bbff8a44479
M-T-Asagi <t.asagi00+github@gmail.com>
2019-03-16 01:06:43 +0900

    スタート画面からの意向を作成

---
+++ Assets/Scripts/Classes/StartPanelToGameMainSceneSwitcher.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class StartPanelToGameMainSceneSwitcher : MonoBehaviour
+{
+    [SerializeField]
+    SceneObject scene;
+
+    public void ChangeScene()
+    {
+        SceneManager.LoadScene(scene, LoadSceneMode.Single);   
+    }
+}
---
+++ Assets/Scripts/Classes/TimeManager.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class TimeManager : MonoBehaviour
+{
+    public class GameFinishEventArgs : System.EventArgs
+    {
+
+    }
+
+    [SerializeField]
+    int totalSeconds = 90;
+
+    [SerializeField]
+    TextMeshProUGUI reamingTimeText;
+
+    public System.EventHandler<GameFinishEventArgs> gameFinish;
+
+    float startTime = 0;
+    bool gaming = false;
+
+    public void StartGame()
+    {
+        Debug.Log("Game started!");
+
+        gaming = true;
+        startTime = Time.time;
+
+        string newTime = (totalSeconds / 60) + ":" + (totalSeconds % 60);
+        reamingTimeText.text = newTime;
+
+        gameFinish += (object sender, GameFinishEventArgs args) =>
+        {
+            Debug.Log("Game is end");
+        };
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float timeNow = Time.time;
+        if(gaming)
+        {
+            int elapsedTime = (int)(timeNow - startTime);
+
+            int reamingTime = totalSeconds - elapsedTime;
+            string newTime = (reamingTime > 0) ? (reamingTime / 60) + ":" + (reamingTime % 60) : "0:00";
+            reamingTimeText.text = newTime;
+
+            if (reamingTime <= 0)
+            {
+                FinishGaming();
+            }
+        }
+    }
+
+    public void FinishGaming()
+    {
+        gaming = false;
+        gameFinish?.Invoke(this, new GameFinishEventArgs());
+    }
+
+    private void OnGUI()
+    {
+        if(GUI.Button(new Rect(0, 500, 100, 100), "Game Start"))
+        {
+            StartGame();
+        }
+    }
+}
