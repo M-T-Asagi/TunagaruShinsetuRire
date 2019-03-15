c8df27c6ad3a5c8dc0996cd589c18037d815ed7a
M-T-Asagi <t.asagi00+github@gmail.com>
2019-03-16 04:44:50 +0900

    自動でアイテムの設置をするスクリプトを追加

---
+++ Assets/Scripts/Classes/ItemCharacterArranger.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemCharacterArranger : MonoBehaviour
+{
+    [System.Serializable]
+    public class ItemCharacterPair
+    {
+        public GameObject Character;
+        public GameObject Item;
+    }
+
+    [SerializeField]
+    List<ItemCharacterPair> itemCharacterPairList = null;
+
+    List<Transform> children;
+    List<int> reamingBoxIndexes;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        children = new List<Transform>();
+        reamingBoxIndexes = new List<int>();
+        foreach (Transform child in transform)
+        {
+            children.Add(child);
+            reamingBoxIndexes.Add(reamingBoxIndexes.Count);
+        }
+        
+        for(int i = 0; i < 4; i++)
+        {
+            int characterItemIndex = Random.Range(0, itemCharacterPairList.Count - 1);
+            
+            int indexBuff = Random.Range(0, reamingBoxIndexes.Count - 1);
+            Instantiate(itemCharacterPairList[characterItemIndex].Character, children[reamingBoxIndexes[indexBuff]]);
+            reamingBoxIndexes.RemoveAt(indexBuff);
+
+            indexBuff = Random.Range(0, reamingBoxIndexes.Count - 1);
+            Instantiate(itemCharacterPairList[characterItemIndex].Item, children[reamingBoxIndexes[indexBuff]]);
+            reamingBoxIndexes.RemoveAt(indexBuff);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
