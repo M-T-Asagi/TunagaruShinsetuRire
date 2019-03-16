69e04c4b7dc6c32fa4f798919e8e8749ddc1300e
M-T-Asagi <t.asagi00+github@gmail.com>
2019-03-17 02:31:48 +0900

    キャラクターモデルの差し替え

---
+++ Assets/Scripts/Classes/LookToBackPowaPowa.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LookToBackPowaPowa : MonoBehaviour
+{
+    [SerializeField]
+    Transform powapowa;
+
+    void Start()
+    {
+        powapowa.LookAt(Camera.main.transform.position, Vector3.up);
+    }
+}
