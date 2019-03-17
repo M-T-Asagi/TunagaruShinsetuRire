e31844c66354acd7512d7ccdc8b824a79c9719e5
M-T-Asagi <t.asagi00+github@gmail.com>
2019-03-17 18:53:37 +0900

    足音を追加

---
+++ Assets/Scripts/Classes/CrawlerWalkSEPlayer.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CrawlerWalkSEPlayer : MonoBehaviour
+{
+    [SerializeField]
+    AudioSource audioSource;
+    [SerializeField]
+    List<AudioClip> actuators;
+    [SerializeField]
+    List<AudioClip> footsteps;
+
+    public void MoveFoot()
+    {
+        audioSource.PlayOneShot(actuators[Random.Range(0, actuators.Count)]);
+    }
+
+    public void FootStep()
+    {
+        audioSource.PlayOneShot(footsteps[Random.Range(0, footsteps.Count)]);
+    }
+}
