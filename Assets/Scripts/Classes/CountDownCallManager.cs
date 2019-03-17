868325ad5717fe082dec903035d1799d8667b90c
M-T-Asagi <t.asagi00+github@gmail.com>
2019-03-17 19:42:33 +0900

    add voice

---
+++ Assets/Scripts/Classes/CountDownCallManager.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CountDownCallManager : MonoBehaviour
+{
+    [SerializeField]
+    AudioSource audioSource;
+
+    [SerializeField]
+    List<AudioClip> voice = null;
+
+    public void CallCount(int index)
+    {
+        audioSource.PlayOneShot(voice[index]);
+    }
+}
